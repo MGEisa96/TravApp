834e22ebe437fdb792c5123313930bc6b440537a
Mohamed Eisa <mge3696@gmail.com>
2025-09-24 22:45:41 +0300

    Refactor code structure for improved readability and maintainability and close new arc

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native';
+import {View,Text} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Screen, {ScreenTypes} from '../../components/organisms/wrapperScreen';
 import {useThemeStore} from '../../zustand/useThemeStore';
